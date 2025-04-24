f0e7555e3ceabe3e7618b091b66d9eb36686a736
Nino Hägglund <ninomh99@gmail.com>
2025-04-24 12:59:38 +0200

    feat: enhance SignIn and Unauthorized pages with improved layout and functionality

--- src/partials/pages/auth/Unauthorized.jsx
+++ src/partials/pages/auth/Unauthorized.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Unauthorized = () => {
   return (
-    <div>Unauthorized</div>
+    <figure className="diff aspect-16/9" tabIndex={0}>
+  <div className="diff-item-1" role="img" tabIndex={0}>
+    <div className="bg-primary text-primary-content grid place-content-center text-9xl font-black">
+      Hello, You are 👉
+    </div>
+  </div>
+  <div className="diff-item-2" role="img">
+    <div className=" bg-error grid place-content-center text-9xl font-black">Unauthorized ⛔</div>
+  </div>
+  <div className="diff-resizer"></div>
+</figure>
   )
 }
 
