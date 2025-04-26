038e2892a1dfae58ab1aecaeaab5bbf4ab512e14
Alec Christensen <alec_christensen@hotmail.com>
2025-04-26 23:22:59 +0200

    Integrerat Header-komponenten i PortalLayout

--- src/partials/layouts/PortalLayout.jsx
+++ src/partials/layouts/PortalLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '../../components/Header/Header'
 
 const PortalLayout = () => {
   return (
     <div className="portal-layout">
+      <Header title="Dashboard" />
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default PortalLayout
+export default PortalLayout;
