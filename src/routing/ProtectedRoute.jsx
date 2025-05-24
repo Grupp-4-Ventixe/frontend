05590f1574bbdc6edbedf75cf91fc520b7dfd696
Nino Hägglund <ninomh99@gmail.com>
2025-05-24 13:34:07 +0200

    Refactor authentication context and routing to enhance user role management and protect routes

--- src/routing/ProtectedRoute.jsx
+++ src/routing/ProtectedRoute.jsx
@@ -2,18 +2,16 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 
-const ProtectedRoute = ({children}) => {
-    try {
-        const { isAuthenticated } = useAuth()
-     
-        if (isAuthenticated && isAuthenticated !== undefined) {
-            return children
-        }
-    
-    }
-    catch { }
+const ProtectedRoute = ({ children }) => {
+    const { isAuthenticated } = useAuth();
 
-    return <Navigate to="/login" replace />
-}
+    
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />;
+    }
+
+
+    return children;
+};
 
 export default ProtectedRoute
